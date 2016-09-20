73d6a7877d4ce46b50a867e2cec0d46bede010ff
pavlix <pavlix@pavlix.net>
2016-09-20 10:32:48 +0200

    python/i: improve input/output examples

--- i/06-input-output-files.examples/readfile.py
+++ i/06-input-output-files.examples/readfile.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
+# encoding: utf-8
+
+from __future__ import print_function
 
 if __name__ == '__main__':
     texts = {}
     with open("file.txt") as stream:
         for line in stream:
-            # Empty line
-            if not line:
-                continue
-            # Comment
-            if line.startswith("#"):
+            # Comment or empty line
+            if line.startswith("#") or line.startswith("\n"):
                 continue
             # Data line
-            lang, text = (item.strip() for item in line.split(":", 1))
-            texts[lang] = text
+            lang, text = line.split(":", 1)
+            texts[lang.strip()] = text.strip()
     print(texts)
