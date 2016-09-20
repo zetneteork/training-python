727da86e7a3e28b4a9e4591e85ba4095c4365a04
Lukáš Viktora <lukas@viktora.cz>
2016-09-20 12:18:18 +0200

    minor changes, internal var

--- i/09-inheritance.examples/shapes.py
+++ i/09-inheritance.examples/shapes.py
@@ -5,21 +5,24 @@ class Shape:
         # default color
         self.color = 'black'
 
+
 class Rectangle(Shape):
     def __init__(self, width, height):
         Shape.__init__(self)
-        self.width = width
-        self.height = height
+        self._width = width
+        self._height = height
 
     def perimeter(self):
-        return 2 * (self.width + self.height)
+        return 2 * (self._width + self._height)
 
     def area(self):
-        return self.width * self.height
+        return self._width * self._height
+
 
 class Square(Rectangle):
     def __init__(self, side):
         Rectangle.__init__(self, side, side)
+
 
 if __name__ == '__main__':
     square = Square(4)
